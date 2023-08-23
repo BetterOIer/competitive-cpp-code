6c7b948cbfdf2f34803bdfae4c0d846ff6b8d691
Better_OIer <2155173747@qq.com>
2023-08-23 14:29:38 +0800

    202308231429

--- Competition/Competition76/A_众数_20181006_A.cc
+++ Competition/Competition76/A_众数_20181006_A.cc
@@ -11,11 +11,23 @@
 #include<map>
 using namespace std;
 inline int read(){int x=0,f=1;char c=getchar();for(;!isdigit(c);c=getchar())if(c=='-')f=-1;for(;isdigit(c);c=getchar())x=(x<<3)+(x<<1)+(c^48);return x*f;}
-int n,m;
+int n,m,idx/* 处理数的离散化 */;
 int a[100005];
+priority_queue<int> q[100005];/* 为当前次数的数们 */
+int tmp[200005]/* 这个离散化后的数的次数 */;
+map<int ,int >M,Mm;
 int main(){
     n=read(),m=read();
     for(int i = 1;i<=n;i++) a[i]=read();
+    for(int i = 1;i<=n;i++){
+        if(!M[a[i]])M[a[i]]=++idx;
+        if(!tmp[M[a[i]]]){
+            tmp[M[a[i]]]++;
+            q[tmp[M[a[i]]]].push(M[a[i]]);
+        }else{
+            q[tmp[M[a[i]]]]
+        }
+    }
     fclose(stdin);
     fclose(stdout);
     return 0;
--- Competition/Competition76/B_绝地求生_20181005_A.cpp
+++ Competition/Competition76/B_绝地求生_20181005_A.cpp
@@ -10,11 +10,17 @@
 using namespace std;
 inline int read(){int x=0,f=1;char c=getchar();for(;!isdigit(c);c=getchar())if(c=='-')f=-1;for(;isdigit(c);c=getchar())x=(x<<3)+(x<<1)+(c^48);return x*f;}
 int x1,y1,x2,y2,a1,a2;
+int gcd(int m, int n){
+    while (n != 0){
+        int t = m % n;
+	    m = n;
+	    n = t;
+	}
+    return m;
+}
 int main(){
-    freopen("pubg.in","r",stdin);
-    freopen("pubg.out","w",stdout);
     while(cin>>x1>>y1>>x2>>y2){
-        if(abs(x2-x1)<=1&&abs(y2-y1)<=1){
+        if(gcd(abs(x1-x2),abs(y1-y2))==1){
             cout<<"YE5\n";
             a1++;
         }else{
@@ -25,7 +31,5 @@ int main(){
     if(a1>a2) cout<<"Poor xshen!";
     else if(a1==a2) cout<<"Friend Ship.";
     else cout<<"Yahoo!";
-    fclose(stdin);
-    fclose(stdout);
     return 0;
 }
