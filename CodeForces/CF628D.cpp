992f33f34c692a9e2c4c45a957ac5ca7459857f8
Better_OIer <2155173747@qq.com>
2023-08-23 21:42:50 +0800

    202308232142

--- CodeForces/CF628D.cpp
+++ CodeForces/CF628D.cpp
@@ -12,11 +12,13 @@ const int mod = 1e9+7;
 inline int read(){int x=0,f=1;char c=getchar();for(;!isdigit(c);c=getchar())if(c=='-')f=-1;for(;isdigit(c);c=getchar())x=(x<<3)+(x<<1)+(c^48);return x*f;}
 int m,d;
 int dp[2005][2005];
+int a[2005];
 string l,r;
 int dfs(int pos,int x,bool lim){
     if(pos==0) return !x;
     if(!lim&&dp[pos][x]!=-1)return dp[pos][x];
-};
+    int high = lim? a[pos]:9;
+}
 int main(){
     m=read(),d=read(),l=read(),r=read();
     cin>>l>>r;
