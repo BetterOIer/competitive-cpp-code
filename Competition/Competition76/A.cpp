fa5911a60d2b2f301b4c22e2110feca1df68a0aa
Better_OIer <2155173747@qq.com>
2023-08-23 14:52:01 +0800

    202308231452

--- Competition/Competition76/A.cpp
+++ Competition/Competition76/A.cpp
@@ -7,28 +7,39 @@
 * OnlineJudge: http://betteroier.site:8888
 =================================================*/
 #include<iostream>
-#include<queue>
 #include<map>
+#include<algorithm>
 using namespace std;
 inline int read(){int x=0,f=1;char c=getchar();for(;!isdigit(c);c=getchar())if(c=='-')f=-1;for(;isdigit(c);c=getchar())x=(x<<3)+(x<<1)+(c^48);return x*f;}
-int n,m,idx/* 处理数的离散化 */;
-int a[100005];
-priority_queue<int> q[100005];/* 为当前次数的数们 */
-int tmp[200005]/* 这个离散化后的数的次数 */;
-map<int ,int >M,Mm;
+int n,m,idx=0;
+int a[100005],b[100005];
+map<int,int>M;
+struct node{
+    int a;
+    int b;
+}tmp[100005];
+bool operator < (node a, node b){
+    if(a.b==b.b)return a.a<b.a;
+    return a.b>b.b;
+}
 int main(){
     n=read(),m=read();
-    for(int i = 1;i<=n;i++) a[i]=read();
     for(int i = 1;i<=n;i++){
-        if(!M[a[i]])M[a[i]]=++idx;
-        if(!tmp[M[a[i]]]){
-            tmp[M[a[i]]]++;
-            q[tmp[M[a[i]]]].push(M[a[i]]);
-        }else{
-            
-        }
+        a[i]=read();
     }
-    fclose(stdin);
-    fclose(stdout);
+    for(int i = 1,x,y;i<=m;i++){
+        x=read(),y=read();
+        a[x]=y;M.clear();idx=0;
+        for(int i = 1;i<=n;i++){
+            if(!M[a[i]])M[a[i]]=0;
+            M[a[i]]++;
+        }
+        for(auto& i:M){
+            tmp[++idx].a=i.first;
+            tmp[idx].b=i.second;
+        }
+        sort(tmp+1,tmp+idx+1);
+        cout<<tmp[1].a<<endl;
+    }
     return 0;
 }
