5a8fb17764c94dd2bc027ade748cef5506a1ea2b
Better_OIer <2155173747@qq.com>
2023-08-24 20:58:51 +0800

    202308242058

--- CodeForces/CF628D.cpp
+++ CodeForces/CF628D.cpp
@@ -7,20 +7,39 @@
 * OnlineJudge: http://betteroier.site:8888
 =================================================*/
 #include<iostream>
+#include<cstring>
 using namespace std;
-const int mod = 1e9+7;
-inline int read(){int x=0,f=1;char c=getchar();for(;!isdigit(c);c=getchar())if(c=='-')f=-1;for(;isdigit(c);c=getchar())x=(x<<3)+(x<<1)+(c^48);return x*f;}
-int m,d;
-int dp[2005][2005];
-int a[2005];
+const long long mod = 1e9+7;
+inline long long read(){long long x=0,f=1;char c=getchar();for(;!isdigit(c);c=getchar())if(c=='-')f=-1;for(;isdigit(c);c=getchar())x=(x<<3)+(x<<1)+(c^48);return x*f;}
+long long m,d,len=0;
+long long dp[2005][2005][2][2];
+long long L[2005],R[2005];
 string l,r;
-int dfs(int pos,int x,bool lim){
-    if(pos==0) return !x;
-    if(!lim&&dp[pos][x]!=-1)return dp[pos][x];
-    int high = lim? a[pos]:9;
+long long dfs(long long pos,long long x,bool Rl,bool Ll,bool ji){
+    if(!pos) return !x;
+    if(~dp[pos][x][Ll][Rl])return dp[pos][x][Ll][Rl];
+    long long high = Rl? min(R[pos],9LL):9,low = Ll? max(L[pos],0LL):0;
+    if(ji){
+        long long ans=0;
+        for(long long i = low;i<=high;i++){
+            if(i==d)continue;
+            (ans+=dfs(pos-1,(x*10+i)%m,Rl&(i==high),Ll&(i==low),ji^1))%=mod;
+        }
+        return dp[pos][x][Ll][Rl]=ans;
+    }else{
+        if(d<low||d>high)return dp[pos][x][Ll][Rl]=0;
+        return dp[pos][x][Ll][Rl]=dfs(pos-1,(x*10+d)%m,Rl&(d==high),Ll&(d==low),ji^1);
+    }
+    return dp[pos][x][Ll][Rl];
 }
 int main(){
-    m=read(),d=read(),l=read(),r=read();
-    cin>>l>>r;
+    m=read(),d=read();
+    cin>>l>>r;len=l.length();
+    memset(dp,-1,sizeof dp);
+    for(long long i = 0;i<len;i++){
+        L[len-i]=l[i]^48;
+        R[len-i]=R[i]^48;
+    }
+    cout<<dfs(len,0,1,1,1);
     return 0;
 }
