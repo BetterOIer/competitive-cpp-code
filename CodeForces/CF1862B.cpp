a7fc418e5a939023bae9afdde6a27943eb0d6134
Better_OIer <2155173747@qq.com>
2023-08-25 15:19:45 +0800

    202308251519

---
+++ CodeForces/CF1862B.cpp
@@ -0,0 +1,31 @@
+/*=================================================
+* Le vent se lève, il faut tenter de vivre!
+* Author: Better_OIer Zyx
+* 起风了，唯有努力生存！
+* Blog: https://betteroier.site:1000
+* FileStation: https://betteroier.site:1005
+* OnlineJudge: http://betteroier.site:8888
+=================================================*/
+#include<iostream>
+using namespace std;
+inline int read(){int x=0,f=1;char c=getchar();for(;!isdigit(c);c=getchar())if(c=='-')f=-1;for(;isdigit(c);c=getchar())x=(x<<3)+(x<<1)+(c^48);return x*f;}
+int a[1000005],n,idx=0;
+int main(){
+    int t =read();
+    while(t--){
+        n=read();
+        for(int i = 1;i<=n;i++){
+            a[++idx]=read();
+            if(a[idx]<a[idx-1]){
+                a[++idx]=1;
+                swap(a[idx],a[idx-1]);
+            }
+        }
+        cout<<idx<<endl;
+        for(int i = 1;i<=idx;i++){
+            cout<<a[i]<<" \n"[i==idx];
+        }
+        idx=0;
+    }
+    return 0;
+}
