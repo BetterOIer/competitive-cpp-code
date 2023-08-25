34754147b379b18e43e28d8289920f393ef5d547
Better_OIer <2155173747@qq.com>
2023-08-25 15:02:58 +0800

    202308251502

--- CodeForces/CF1862A.cpp
+++ CodeForces/CF1862A.cpp
@@ -9,8 +9,30 @@
 #include<iostream>
 using namespace std;
 inline int read(){int x=0,f=1;char c=getchar();for(;!isdigit(c);c=getchar())if(c=='-')f=-1;for(;isdigit(c);c=getchar())x=(x<<3)+(x<<1)+(c^48);return x*f;}
-
+int n,m;
+int have[25];
 int main(){
-    
+    int t=read();
+    while(t--){
+        for(int i = 1;i<=20;i++)have[i]=0;
+        n=read(),m=read();
+        for(int i = 1;i<=n;i++){
+            string s;
+            cin>>s;
+            for(int j = 1;j<=m;j++){
+                if(s[j-1]=='v')have[j]|=1;
+                if(s[j-1]=='i')have[j]|=(1<<1);
+                if(s[j-1]=='k')have[j]|=(1<<2);
+                if(s[j-1]=='a')have[j]|=(1<<3);
+            }
+        }
+        int cho=0;
+        for(int i = 1;i<=m&&cho<4;i++){
+            if(have[i]&(1<<cho)){
+                cho++;
+            }
+        }
+        cout<<(cho==4? "YES\n":"NO\n");
+    }
     return 0;
 }
