5ed400d3e05600a13fecc6c3ca961e8f49e98bee
Better_OIer <2155173747@qq.com>
2023-08-27 08:57:06 +0800

    202308270857

---
+++ Competition/Competition79/B.cc
@@ -0,0 +1,37 @@
+/*=================================================
+* Le vent se lève, il faut tenter de vivre!
+* Author: Better_OIer Zyx
+* 起风了，唯有努力生存！
+* Blog: https://betteroier.site:1000
+* FileStation: https://betteroier.site:1005
+* OnlineJudge: http://betteroier.site:8888
+=================================================*/
+#include<iostream>
+#include<algorithm>
+using namespace std;
+inline int read(){int x=0,f=1;char c=getchar();for(;!isdigit(c);c=getchar())if(c=='-')f=-1;for(;isdigit(c);c=getchar())x=(x<<3)+(x<<1)+(c^48);return x*f;}
+int n,k;
+string s;
+void solve(){
+    bool flag=true;
+    while(flag){
+        flag=false;
+        for(int i = 0;i<=n-3;i++){
+            if(s[i]>s[i+2]){
+                swap(s[i],s[i+2]);
+                flag=true;
+            }
+        }
+    }
+}
+int main(){
+    int t=read();
+    while(t--){
+        n=read(),k=read();
+        cin>>s;
+        if(k&1)solve();
+        else sort(s.begin(),s.end());
+        cout<<s<<endl;
+    }
+    return 0;
+}
