0124ab24ae4c83557af8bd4f8f1ed9bf832374fc
Better_OIer <2155173747@qq.com>
2023-08-29 21:39:20 +0800

    202308292139

--- Luogu/P8435.cpp
+++ Luogu/P8435.cpp
@@ -11,16 +11,11 @@
 #include<stack>
 using namespace std;
 inline int read(){int x=0,f=1;char c=getchar();for(;!isdigit(c);c=getchar())if(c=='-')f=-1;for(;isdigit(c);c=getchar())x=(x<<3)+(x<<1)+(c^48);return x*f;}
-struct node{
-    int to;
-    int a;
-    int b;
-};
-vector<node>ro[25];
-vector<int>dcc[25];
+vector<int>ro[2000005];
+vector<int>dcc[2000005];
 stack<int>stk;
-int dfn[25],low[25],cut[25],tot=0,cnt=0,root;
-bool instack[25];
+int dfn[2000005],low[2000005],cut[2000005],tot=0,cnt=0,root;
+bool instack[2000005];
 inline void Tarjan(int pos){
 	dfn[pos]=low[pos]=++tot;
     stk.push(pos);
@@ -29,32 +24,39 @@ inline void Tarjan(int pos){
         return;
     }
     int child = 0;
-    for(node to:ro[pos]){
-        if(!dfn[to.to]){
-            Tarjan(to.to);
-            low[pos]=min(low[pos],low[to.to]);
-            if(low[to.to]>=low[pos]){
+    for(int to:ro[pos]){
+        if(!dfn[to]){
+            Tarjan(to);
+            low[pos]=min(low[pos],low[to]);
+            if(low[to]>=dfn[pos]){
                 child++;
                 if(pos!=root||child>1)cut[pos]=true;
                 cnt++;
                 while(true){
                     int z = stk.top();stk.pop();
                     dcc[cnt].push_back(z);
-                    if(z==to.to)break;
+                    if(z==to)break;
                 }
                 dcc[cnt].push_back(pos);
             }
-        }else low[pos]=min(low[pos],dfn[to.to]);
+        }else low[pos]=min(low[pos],dfn[to]);
     }
 }
 int main(){
     int n=read(),m=read();
-    for(int i = 1,u,v,a,b;i<=m;i++){
-        u=read(),v=read(),a=read(),b=read();
-        ro[u].emplace_back((node){v,a,b});
-        ro[v].emplace_back((node){u,a,b});
+    for(int i = 1,u,v;i<=m;i++){
+        u=read(),v=read();
+        ro[u].emplace_back(v);
+        ro[v].emplace_back(u);
     }
     for (root=1;root<=n;root++) if (!dfn[root]) Tarjan(root);
-    n = 0;
+    cout<<cnt<<endl;
+    for(int i = 1;i<=cnt;i++){
+        cout<<dcc[i].size();
+        for(int whi:dcc[i]){
+            cout<<" "<<whi;
+        }
+        cout<<endl;
+    }
     return 0;
 }
