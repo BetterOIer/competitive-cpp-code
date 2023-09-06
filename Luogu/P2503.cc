e622470dc1b0fc6bbb52a4ef71ee8fdfcc099465
Better_OIer <2155173747@qq.com>
2023-09-06 08:33:38 +0800

    202309060833

--- Luogu/P2503.cc
+++ Luogu/P2503.cc
@@ -1,24 +1,15 @@
-/*=================================================
-* Le vent se lève, il faut tenter de vivre!
-* Author: Better_OIer Zyx
-* 起风了，唯有努力生存！
-* Blog: https://betteroier.site:1000
-* FileStation: https://betteroier.site:1005
-* OnlineJudge: http://betteroier.site:8888
-=================================================*/
 #include<iostream>
 #include<random>
-#include<ctime>
 #include<algorithm>
 using namespace std;
 inline int read(){int x=0,f=1;char c=getchar();for(;!isdigit(c);c=getchar())if(c=='-')f=-1;for(;isdigit(c);c=getchar())x=(x<<3)+(x<<1)+(c^48);return x*f;}
-int n,m;
+int n,m,t;
 vector<int> validBit;
 int a[22];
 int sum[22];
 int mVal[7];
 double ans=1e9;
-mt19937 eng(time(0));
+mt19937 eng(19491001),eng2(12);
 double getMin(int x){
     int last = 0,now=0,cnt=0,cnt2=0;
     while(x){
@@ -39,8 +30,33 @@ double getMin(int x){
     cnt3 = 1.0*cnt3/m;
     return __builtin_sqrt(cnt3);
 }
+double Rand(){return rand()%100000/100000.00;}
+void SA1(){
+    double temp=10000,down=1e-3,delta=0.99,res=ans;
+    while(temp>down){
+        int
+        x = eng2()%n+1,
+        y = eng2()%n+1;
+        while(y==x)y = eng2()%n+1;
+        swap(a[y],a[x]);
+        for(int i = 1;i<=n;i++){
+            sum[i]=a[i]+sum[i-1];
+        }
+        for(auto i:validBit){
+            res = min(res,getMin(i));
+        }
+        if(res<ans){
+            ans=res;
+        }else{
+            if(exp(ans-res)/temp>Rand()){
+                swap(a[y],a[x]);
+            }
+        }
+        temp*=delta;
+    }
+}
 void SA(){
-    double temp=3000,down=1e-10,delta=0.99,res=ans;
+    double temp=10000,down=1e-3,delta=0.99,res=ans;
     while(temp>down){
         int
         x = eng()%n+1,
@@ -56,7 +72,7 @@ void SA(){
         if(res<ans){
             ans=res;
         }else{
-            if(-exp((res-ans)/temp)*RAND_MAX>rand()){
+            if(exp(ans-res)/temp>Rand()){
                 swap(a[y],a[x]);
             }
         }
@@ -64,7 +80,7 @@ void SA(){
     }
 }
 int main(){
-    int t = clock();
+    t = clock();
     n=read(),m=read();
     for(int i = 1;i<=n;i++){
         a[i]=read();
@@ -72,7 +88,7 @@ int main(){
     for(int i = 1;i<=((1<<(n-1))-1);i++){
         if(__builtin_popcount(i)==m-1)validBit.push_back(i);
     }
-    SA();SA();SA();SA();SA();SA();
+    SA(),SA1();
     printf("%.2lf",ans);
     return 0;
 }
