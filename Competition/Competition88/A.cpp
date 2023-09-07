b0826b1aa1990adb7c0968be5328c687db621b9d
Better_OIer <2155173747@qq.com>
2023-09-07 22:44:21 +0800

    202309072244

---
+++ Competition/Competition88/A.cpp
@@ -0,0 +1,21 @@
+/*=================================================
+* Le vent se lève, il faut tenter de vivre!
+* Author: Better_OIer Zyx
+* 起风了，唯有努力生存！
+* Blog: https://betteroier.site:1000
+* FileStation: https://betteroier.site:1005
+* OnlineJudge: http://betteroier.site:8888
+=================================================*/
+#include<iostream>
+#include<cmath>
+using namespace std;
+inline int read(){int x=0,f=1;char c=getchar();for(;!isdigit(c);c=getchar())if(c=='-')f=-1;for(;isdigit(c);c=getchar())x=(x<<3)+(x<<1)+(c^48);return x*f;}
+int a,b,c;
+int main(){
+    int t=read();
+    while(t--){
+        a=read(),b=read(),c=read();
+        cout<<(int)(ceil(1.0*(abs(a-b))/2/c))<<endl;
+    }
+    return 0;
+}
