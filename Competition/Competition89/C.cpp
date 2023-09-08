0d378021a5e9efe570b36172dc70adb62b0fd6c3
Better_OIer <2155173747@qq.com>
2023-09-08 16:29:59 +0800

    202309081629

--- Competition/Competition89/C.cpp
+++ Competition/Competition89/C.cpp
@@ -6,7 +6,26 @@ inline int read(){
     for(;isdigit(c);c=getchar())x=(x<<3)+(x<<1)+(c^48);
     return x*f;
 }
+int n,c;
+int a[100001],b[100001];
+void dfs(int x,int y){
+    if(x==n&&y==n)return ;
+    if(a[x+1]*b[y]%c>a[x]*b[y+1]%c){
+        cout<<"R";
+        dfs(x+1,y);
+    }else{
+        cout<<"D";
+        dfs(x,y+1);
+    }
+}
 int main(){
-    
+    n=read(),c=read();
+    for(int i = 1;i<=n;i++){
+        a[i]=read();
+    }
+    for(int i = 1;i<=n;i++){
+        b[i]=read();
+    }
+    dfs(1,1);
     return 0;
 }
