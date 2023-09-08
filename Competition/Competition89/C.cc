02c64e50250b7fdf1a1b17f98bc436b201580a8f
Better_OIer <2155173747@qq.com>
2023-09-08 21:01:19 +0800

    202309082101

--- Competition/Competition89/C.cc
+++ Competition/Competition89/C.cc
@@ -1,17 +1,10 @@
-/*=================================================
-* Le vent se lève, il faut tenter de vivre!
-* Author: Better_OIer Zyx
-* 起风了，唯有努力生存！
-* Blog: https://betteroier.site:1000
-* FileStation: https://betteroier.site:1005
-* OnlineJudge: http://betteroier.site:8888
-=================================================*/
 #include<iostream>
+#include<bitset>
 using namespace std;
 inline int read(){int x=0,f=1;char c=getchar();for(;!isdigit(c);c=getchar())if(c=='-')f=-1;for(;isdigit(c);c=getchar())x=(x<<3)+(x<<1)+(c^48);return x*f;}
-int a[101],b[101];
-int dp[101][101];
-int pre[101][101];
+int a[5005],b[5005];
+int dp[5005];
+bitset<5005>pre[5005];
 int n,c;
 void out(int x,int y){
     if(x==y&&x==1){
@@ -31,12 +24,12 @@ int main(){
     for(int i = 1;i<=n;i++) b[i]=read();
     for(int i =  1;i<=n;i++){
         for(int j =  1;j<=n;j++){
-            if(dp[i][j-1]>dp[i-1][j]){
-                dp[i][j]=dp[i][j-1]+a[i]*b[j]%c;
+            if(dp[j-1]>dp[j]){
+                dp[j]=dp[j-1]+a[i]*b[j]%c;
                 pre[i][j]=1;
             }else{
-                dp[i][j]=dp[i-1][j]+a[i]*b[j]%c;
-                pre[i][j]=-1;
+                dp[j]=dp[j]+a[i]*b[j]%c;
+                pre[i][j]=0;
             }
         }
     }
