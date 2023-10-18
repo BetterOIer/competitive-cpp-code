88979827637ccf87634ab349890405638976a949
Better_OIer <2155173747@qq.com>
2023-10-18 15:20:08 +0800

    202310181520

---
+++ Competition/Competition99/ZJ-S01337/string/string.cc
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<cstdio>
+#include<vector>
+#include<algorithm>
+using namespace std;
+inline int read(){
+    int x=0,f=1;char c=getchar();
+    for(;!isdigit(c);c=getchar())if(c=='-')f=-1;
+    for(;isdigit(c);c=getchar())x=(x<<3)+(x<<1)+(c^48);
+    return x*f;
+}
+int n,tot=0,strLen;
+struct node{
+    int num;
+    int son[26];
+}trie[300005];
+string s,str[30005];
+vector<int>ans;
+int pri[26],priTot=-1;
+void insert(int triePos, int strPos,int strId){
+    if(strPos==strLen){
+        trie[triePos].num=strId;
+        return;
+    }
+    int nex = s[strPos]-'a';
+    if(!trie[triePos].son[nex])trie[triePos].son[nex]=++tot;
+    insert(trie[triePos].son[nex], strPos+1,strId);
+}
+void dfs(int pos,int prePri){
+    if(trie[pos].num)return ans.push_back(trie[pos].num);
+    bool vis[26],flag=false;for(int i = 0;i<26;i++)vis[i]=false;
+    for(int i = 0;i<=priTot;i++){
+        vis[pri[i]]=true;
+        if((!flag)&&trie[pos].son[pri[i]]){dfs(trie[pos].son[pri[i]],i);flag=true;}
+    }
+    priTot = prePri+1;
+    for(int i = 0;i<26;i++){
+        if(vis[i])continue;
+        if(trie[pos].son[i]){
+            pri[priTot]=i;
+            dfs(trie[pos].son[i],priTot);
+        }
+    }
+}
+int main(){
+    /* freopen("string.in","r",stdin);
+    freopen("string.out","w",stdout); */
+    n=read();
+    for(int i = 1;i<=n;i++){
+        cin>>s;strLen = s.length();
+        insert(0,0,i);
+        str[i]=s;
+    }
+    dfs(0,-1);
+    sort(ans.begin(),ans.end());
+    cout<<ans.size()<<endl;
+    for(int i:ans){
+        cout<<str[i]<<endl;
+    }
+    fclose(stdin);
+    fclose(stdout);
+    return 0;
+}
--- Competition/Competition99/ZJ-S01337/string/string.cpp
+++ Competition/Competition99/ZJ-S01337/string/string.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include<cstdio>
-#include<vector>
+#include<set>
 #include<algorithm>
 using namespace std;
 inline int read(){
@@ -9,53 +9,37 @@ inline int read(){
     for(;isdigit(c);c=getchar())x=(x<<3)+(x<<1)+(c^48);
     return x*f;
 }
-int n,tot=0,strLen;
-struct node{
-    int num;
-    int son[26];
-}trie[300005];
-string s,str[30005];
-vector<int>ans;
-int pri[26],priTot=-1;
-bool vaild[26];
-void insert(int triePos, int strPos,int strId){
-    if(strPos==strLen){
-        trie[triePos].num=strId;
-        return;
+int n;
+string str[55];
+int id[55];
+bool apper[26];
+int pri[9]={0,1,2,3,4,5,6,7,8};
+set<int>ans;
+bool cmp(int ida,int idb){
+    int lena = str[ida].length();
+    int lenb = str[idb].length();
+    string sta = str[ida],stb=str[idb];
+    while(lena<lenb)sta+='`',lena++;
+    while(lenb<lena)stb+='`',lenb++;
+    for(int i = 0;i<lena;i++){
+        if(pri[sta[i]-'`']<pri[stb[i]-'`'])return true;
+        else if(pri[sta[i]-'`']>pri[stb[i]-'`'])return false;
     }
-    int nex = s[strPos]-'a';
-    if(!trie[triePos].son[nex])trie[triePos].son[nex]=++tot;
-    insert(trie[triePos].son[nex], strPos+1,strId);
-}
-void dfs(int pos,int prePri){
-    if(trie[pos].num)return ans.push_back(trie[pos].num),void();
-    bool vis[26];for(int i = 0;i<26;i++)vis[i]=false;
-    for(int i = 0;i<=priTot;i++){
-        vis[pri[i]]=true;
-        if(trie[pos].son[pri[i]])dfs(trie[pos].son[pri[i]],i);
-    }
-    for(int i = prePri+1;i<=priTot;i++){
-        vaild[pri[i]]=true;
-    }
-    priTot = prePri+1;
-    for(int i = 0;i<26;i++){
-        if(vis[i])continue;
-        if(trie[pos].son[i]&&vaild[i]){
-            pri[priTot]=i;vaild[i]=false;
-            dfs(trie[pos].son[i],priTot);
-        }
-    }
+    
 }
 int main(){
+    freopen("string.in","r",stdin);
+    freopen("string.out","w",stdout);
     n=read();
     for(int i = 1;i<=n;i++){
-        cin>>s;strLen = s.length();
-        insert(0,0,i);
-        str[i]=s;
+        cin>>str[i];id[i]=i;
     }
-    for(int i = 0;i<26;i++)pri[i]=-1;
-    dfs(0,-1);
-    sort(ans.begin(),ans.end());
+    do{
+        /* for(int i = 0;i<=8;i++)cout<<pri[i]<<" \n"[i==8]; */
+        sort(id+1,id+n+1,cmp);
+        ans.insert(id[1]);
+    }while(next_permutation(pri+1,pri+9));
+    cout<<ans.size()<<endl;
     for(int i:ans){
         cout<<str[i]<<endl;
     }
