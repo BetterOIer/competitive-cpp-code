49174b758188858b22f28244723c3599548b8b75
Better_OIer <2155173747@qq.com>
2023-10-24 10:02:24 +0800

    202310241002

--- Competition/Competition100/ZJ-S01337/tree/tree.cpp
+++ Competition/Competition100/ZJ-S01337/tree/tree.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include<cstdio>
-#include<vector>
+#include<queue>
 using namespace std;
 inline int read(){
     int x=0,f=1;char c=getchar();
@@ -8,9 +8,13 @@ inline int read(){
     for(;isdigit(c);c=getchar())x=(x<<1)+(x<<3)+(c^48);
     return x*f;
 }
-int a[100005],b[100005],c[100005],plaDead[100005];
+int a[100005],b[100005],c[100005],plaDead[100005],n;
 vector<int>ro[100005];
-int calDead(int pos,int deadLine){
+priority_queue<pair<int, int> >Q;
+bool operator < (pair<long long, long long> a, pair<long long, long long > b){
+    return a.second<b.second;
+}
+/* int calDead(int pos,int deadLine){
     int hei = a[pos];
     int l = 1,r=deadLine;
     if(c[pos]>=0||deadLine<-b[pos]/c[pos]){
@@ -29,6 +33,38 @@ int calDead(int pos,int deadLine){
             }else l = mid+1;
         }
     }
+} */
+int cal1(int las,long long b,long long c,__int128 a){
+    __int128 h2=b+las*c;
+    if(h2>=a)return n;
+    int l=0,r=n;
+    while(l<r){
+        int mid=(l+r+1>>1);
+        if((__int128)(h2+b+c*mid)*(las-mid+1)>=a*2)l=mid;//A.P
+        else r=mid-1;
+    }
+    return l;
+}
+int cal2(int las,long long b,long long c,__int128 a){
+    long long o=(b-1)/(-c);
+    int l=0,r=n;
+    while(l<r){
+        int mid=(l+r+1>>1);
+        __int128 S;
+        if(mid>o)S=las-mid+1;
+        else if(las>o)S=(__int128)((__int128)b*2+c*(o+mid))*(o-mid+1)/2+(las-o);
+        else S=(__int128)((__int128)b*2+c*(las+mid))*(las-mid+1)/2;
+        if(S>=a)l=mid;
+        else r=mid-1;
+    }
+    return l;
+}
+int calDead(int pos,int deadLine){
+    if(c[pos]>=0){
+        return cal1(deadLine,b[pos],c[pos],a[pos]);
+    }else{
+        return cal2(deadLine,b[pos],c[pos],a[pos]);
+    }
 }
 int dfs(int pos,int fa,int deadLine){
     int myDead = calDead(pos,deadLine);
@@ -40,11 +76,15 @@ int dfs(int pos,int fa,int deadLine){
 }
 bool vaild(int deadLine){
     dfs(1,0,deadLine);
+    Q.push(make_pair(1,1));
+    while(Q.size()){
+        
+    }
 }
 int main(){
     freopen("tree.in","r",stdin);
     freopen("tree.out","w",stdout);
-    int n=read();
+    n=read();
     for(int i  = 1;i<=n;i++){
         a[i]=read(),b[i]=read(),c[i]=read();
     }
@@ -53,7 +93,7 @@ int main(){
         ro[u].push_back(v);
         ro[v].push_back(u);
     }
-    int l = 1,r = 1e9;
+    int l = n,r = 1e9;
     while(l<=r){
         int mid = (l+r)>>1;
         if(vaild(mid)){
