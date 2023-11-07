a3d3b823dc8c32450aba7e111e72926ac856476c
Better_OIer <2155173747@qq.com>
2023-11-07 15:09:11 +0800

    202311071509

--- Competition/Competition107/lis.cc
+++ Competition/Competition107/lis.cc
@@ -3,8 +3,8 @@
 using namespace std;
 inline int read(){int x=0,f=1;char c=getchar();for(;!isdigit(c);c=getchar())if(c=='-')f=-1;for(;isdigit(c);c=getchar())x=(x<<3)+(x<<1)+(c^48);return x*f;}
 int a[505],n,maxC,ans[250005];
-int dp[505][505][505];
-int minn[505][505][505];
+int dp[2][505][505];
+int minn[2][505][505];
 int main(){
     #ifndef LOCAL
     freopen("lis.in","r",stdin);
@@ -15,16 +15,18 @@ int main(){
     for(int i = 1;i<=n;i++){
         a[i]=read();
     }
-    for(int i = 1;i<=n;i++){
+    int pos=0;
+    for(int i = 1;i<=n;i++,pos^=1){
         for(int j = 1;j<=i;j++){
-            minn[i][j][0]=0x3f3f3f3f;
+            minn[pos][j][0]=0x3f3f3f3f;
             for(int v = 1;v<=n;v++){
-                dp[i][j][v]=min(dp[i-1][j][v],minn[i-1][j-1][v-1]+abs(a[i]-v));
-                minn[i][j][v]=min(dp[i][j][v],minn[i][j][v-1]);
+                dp[pos][j][v]=min(dp[pos^1][j][v],minn[pos^1][j-1][v-1]+abs(a[i]-v));
+                minn[pos][j][v]=min(dp[pos][j][v],minn[pos][j][v-1]);
             }
         }
     }
-    for(int j = 1;j<=n;j++)if(minn[n][j][n]<=maxC)ans[minn[n][j][n]]=j;
+    pos^=1;
+    for(int j = 1;j<=n;j++)if(minn[pos][j][n]<=maxC)ans[minn[pos][j][n]]=j;
     for(int i = 0,res=0;i<=maxC;i++){
         res = max(res,ans[i]);cout<<res<<" ";
     }
