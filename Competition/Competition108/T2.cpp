2050d09308d46b9c45c3b606fca2ced027e36513
Better_OIer <2155173747@qq.com>
2023-11-08 12:43:12 +0800

    202311081243

--- Competition/Competition108/T2.cpp
+++ Competition/Competition108/T2.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 inline int read(){int x=0,f=1;char c=getchar();for(;!isdigit(c);c=getchar())if(c=='-')f=-1;for(;isdigit(c);c=getchar())x=(x<<3)+(x<<1)+(c^48);return x*f;}
 int n,k,id;
-int idx[25],val[25];
-bool isBig[25];
+int idx[2000005],val[2000005];
+bool isBig[2000005];
 struct node{int to;int val;};
-vector<node>ro[25];
-vector<node>nRo[25];
+vector<node>ro[2000005];
+vector<node>nRo[2000005];
 int dfs(int pos,int fa,int roval){
     if(fa==0)idx[pos]=++id;
     if(ro[pos].size()<=k)idx[pos]=id;
@@ -20,6 +20,13 @@ int dfs(int pos,int fa,int roval){
     return res+roval;
 }
 int main(){
+    #ifndef LOCAL
+    freopen("warm.in","r",stdin);
+    freopen("warm.out","w",stdout);
+    #else
+    freopen("ex_contest3.in","r",stdin);
+    freopen("ex_contest3.out","w",stdout);
+    #endif
     n=read(),k=read();
     for(int i = 1,u,v,w;i<n;i++){
         u=read(),v=read(),w=read();
