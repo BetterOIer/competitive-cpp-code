6fd36bcce9a01d74a248adfcfb2476037c68e8b6
Better_OIer <2155173747@qq.com>
2023-11-13 12:31:15 +0800

    202311131231

--- Competition/Competition113/travel.cc
+++ Competition/Competition113/travel.cc
@@ -1,6 +1,6 @@
 #include<iostream>
 #include<vector>
-#include<algorithm>
+#include<queue>
 #include<cstring>
 using namespace std;
 inline long long read(){long long x=0,f=1;char c=getchar();for(;!isdigit(c);c=getchar())if(c=='-')f=-1;for(;isdigit(c);c=getchar())x=(x<<3)+(x<<1)+(c^48);return x*f;}
@@ -8,26 +8,26 @@ long long n,m,q;
 struct node{long long to;long long val;};
 vector<node>ro[100005];
 long long sup[100005];
-bool vis[100005];
-long long dfs(long long pos,int v){
+long long vis[100005],tot=0;
+bool ter[100005];
+struct Qnode{long long pos;long long val;};
+queue<Qnode>Q;
+void bfs(long long x,long long k){
+    memset(vis,0,sizeof vis);
     long long ans=0;
-    vis[pos]=true;
-    for(auto i:ro[pos]){
-        if(vis[i.to]||i.val>v)continue;
-        ans+=dfs(i.to,v);
+    Q.push((Qnode){x,k});
+    while(Q.size()){
+        Qnode now = Q.front();Q.pop();
+        if(!vis[now.pos])now.val+=sup[now.pos],vis[now.pos]=now.val;
+        else if(now.val==vis[now.pos]){ter[now.pos]=true;tot++;if(tot==n)break;continue;}
+        vis[now.pos]=max(vis[now.pos],now.val);
+        ans=max(ans,now.val);
+        for(node i:ro[now.pos]){
+            if(now.val>=i.val&&!ter[i.to])Q.push((Qnode){i.to,now.val});
+        }
     }
-    ans+=sup[pos];
-    return ans;
-}
-void solv(long long x,long long k){
-    long long nowVal=k+sup[x];
-    while(true){
-        memset(vis,0,sizeof vis);
-        int nn=dfs(x,nowVal)+k;
-        if(nn==nowVal)break;
-        nowVal=nn;
-    }
-    cout<<nowVal<<endl;
+    while(Q.size())Q.pop();
+    cout<<ans<<endl;
 }
 int main(){
     #ifndef LOCAL
@@ -46,10 +46,9 @@ int main(){
         ro[u].push_back((node){v,w});
         ro[v].push_back((node){u,w});
     }
-/*     sort(edge.begin(),edge.end()); */
     for(long long i = 1,x,k;i<=q;i++){
         x=read(),k=read();
-        solv(x,k);
+        bfs(x,k);
     }
     return 0;
 }
