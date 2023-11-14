fb81f048e8d013c52335c7f12b71a71b3ab629da
Better_OIer <2155173747@qq.com>
2023-11-14 11:08:54 +0800

    202311141108

--- Competition/Competition114/masses.cpp
+++ Competition/Competition114/masses.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 inline int read(){int x=0,f=1;char c=getchar();for(;!isdigit(c);c=getchar())if(c=='-')f=-1;for(;isdigit(c);c=getchar())x=(x<<3)+(x<<1)+(c^48);return x*f;}
-
+int n,m,t,up,down;
+int hap[100005];
+vector<int>ro[100005];
 int main(){
     #ifndef LOCAL
     freopen("masses.in","r",stdin);
@@ -10,6 +13,10 @@ int main(){
     freopen("exmasses_2.in","r",stdin);
     freopen("exmasses_2.res","w",stdout);
     #endif
-    
+    scanf("%d %d %d %d/%d",&n,&m,&t,&up,&down);
+    for(int i = 1;i<=n;i++)hap[i]=read();
+    for(int i = 1,u,v;i<=m;i++){
+        u=read()
+    }
     return 0;
 }
--- Competition/Competition114/travel.cpp
+++ Competition/Competition114/travel.cpp
@@ -3,20 +3,20 @@
 // #include<queue>
 // #include<map>
 // using namespace std;
-// inline int read(){int x=0,f=1;char c=getchar();for(;!isdigit(c);c=getchar())if(c=='-')f=-1;for(;isdigit(c);c=getchar())x=(x<<3)+(x<<1)+(c^48);return x*f;}
+// inline long long read(){long long x=0,f=1;char c=getchar();for(;!isdigit(c);c=getchar())if(c=='-')f=-1;for(;isdigit(c);c=getchar())x=(x<<3)+(x<<1)+(c^48);return x*f;}
 // struct node{
-//     int to;
-//     int val;
+//     long long to;
+//     long long val;
 // };
 // vector<node>ro[1005];
-// deque<int>stk;
-// vector<int>scc[1005],rscc[1005];
-// map<pair<int, int>, int >edge;
+// deque<long long>stk;
+// vector<long long>scc[1005],rscc[1005];
+// map<pair<long long, long long>, long long >edge;
 // bool instack[1005];
-// int n,m,k,q;
-// int dfn[1005],tot=0,cnt=0;
-// int val[1005];
-// void Tarjan(int pos){
+// long long n,m,k,q;
+// long long dfn[1005],tot=0,cnt=0;
+// long long val[1005];
+// void Tarjan(long long pos){
 //     dfn[pos]=++tot;
 //     stk.push_front(pos);
 //     instack[pos]=true;
@@ -24,7 +24,7 @@
 //         if(!dfn[to.to]){
 //             Tarjan(to.to);
 //         }else if(instack[to.to]){
-//             int y,last=to.to;++cnt;
+//             long long y,last=to.to;++cnt;
 //             do{
 //                 y=stk.front();stk.pop_front();
 //                 instack[y]=false,scc[cnt].push_back(y),rscc[y].push_back(cnt),val[cnt]+=edge[make_pair(y,last)],last=y;
@@ -33,7 +33,7 @@
 //         }
 //     }
 // }
-// int main(){
+// long long main(){
 //     #ifndef LOCAL
 //     freopen("travel.in","r",stdin);
 //     freopen("travel.out","w",stdout);
@@ -41,18 +41,18 @@
 //     /* freopen("extravel_2.in","r",stdin);
 //     freopen("extravel_2.res","w",stdout); */
 //     #endif
-//     int n=read(),m=read(),k=read(),q=read();
-//     for(int i = 1,u,v,w;i<=m;i++){
+//     long long n=read(),m=read(),k=read(),q=read();
+//     for(long long i = 1,u,v,w;i<=m;i++){
 //         u=read(),v=read(),w=read();
 //         ro[u].push_back((node){v,w});
 //         edge[make_pair(u,v)]=w;
 //     }
 //     Tarjan(1);
 //     while(stk.size()){
-//         int y=stk.front();stk.pop_front();++cnt;
+//         long long y=stk.front();stk.pop_front();++cnt;
 //         instack[y]=false;scc[cnt].push_back(y),rscc[y].push_back(cnt);
 //     }
-//     for(int i = 1;i<=n;i++){
+//     for(long long i = 1;i<=n;i++){
 //         if(rscc[i].size()>1)
 //     }
 //     return 0;
@@ -66,19 +66,19 @@
 #include<queue>
 #include<set>
 using namespace std;
-inline int read(){int x=0,f=1;char c=getchar();for(;!isdigit(c);c=getchar())if(c=='-')f=-1;for(;isdigit(c);c=getchar())x=(x<<3)+(x<<1)+(c^48);return x*f;}
+inline long long read(){long long x=0,f=1;char c=getchar();for(;!isdigit(c);c=getchar())if(c=='-')f=-1;for(;isdigit(c);c=getchar())x=(x<<3)+(x<<1)+(c^48);return x*f;}
 struct node{
-    int pos;
-    int val;
+    long long pos;
+    long long val;
 };
-int n,m,k,q;
+long long n,m,k,q;
 vector<node>ro[1005];
-int vis[1005];
-set<int>val[1005];
-void solve(int pos){
+long long vis[1005];
+set<long long>val[1005];
+void solve(long long pos){
     queue<node>Q;while(Q.size())Q.pop();
-    for(int i = 1;i<=n;i++)val[i].clear();
-    for(int i = 1;i<=n;i++)vis[i]=0;
+    for(long long i = 1;i<=n;i++)val[i].clear();
+    for(long long i = 1;i<=n;i++)vis[i]=0;
     Q.push(node{1,k});
     while(Q.size()){
         node now = Q.front();Q.pop();
@@ -87,7 +87,7 @@ void solve(int pos){
         else continue;
         if(!vis[now.pos]){
             vis[now.pos]=now.val;
-        }else for(int i = now.val-(vis[now.pos]-now.val);i>=0;i-=(vis[now.pos]-now.val))Q.push((node){now.pos,i});
+        }else for(long long i = now.val-(vis[now.pos]-now.val);i>=0;i-=(vis[now.pos]-now.val))Q.push((node){now.pos,i});
         if(now.pos==pos) continue;
         for(node to:ro[now.pos]){
             if(now.val-to.val>=0)Q.push((node){to.pos,now.val-to.val});
@@ -104,11 +104,11 @@ int main(){
     freopen("extravel_2.res","w",stdout);
     #endif
     n=read(),m=read(),k=read(),q=read();
-    for(int i = 1,u,v,w;i<=m;i++){
+    for(long long i = 1,u,v,w;i<=m;i++){
         u=read(),v=read(),w=read();
         ro[u].push_back((node){v,w});
     }
-    for(int i = 1,x;i<=q;i++){
+    for(long long i = 1,x;i<=q;i++){
         x=read();solve(x);
     }
     return 0;
